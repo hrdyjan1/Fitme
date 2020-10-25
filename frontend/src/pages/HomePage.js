c0336f6704279fb4a23dba6be8e80a53e7fd68e1
Jan Hrdý <hrdyjan1@gmail.com>
2020-10-25 15:27:05 +0100

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,7 +4,7 @@ export function HomePage() {
   return (
     <div className="appWrapper">
       <h1>Fit.me</h1>
-      <h2>Contributors</h1>
+      <h2>Contributors</h2>
       <p>Jan Hrdy</p>
       <p>Michael Linka</p>
     </div>
