292d12860d16bdbb48badf7d42a4a1a0e17ab3aa
Jan Hrdý <hrdy@czc.cz>
2020-11-03 17:33:10 +0100

    FM-3 Initialized connection - enabled eslint

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { isFilledArray } from 'src/constants/array';
+
+const GET_PLACES = gql`
+  query GetPlaces {
+    places {
+      id
+      name
+      description
+    }
+  }
+`;
 
 export function HomePage() {
-  const [state, setstate] = React.useState(false);
-
-  React.useEffect(() => {
-    console.log(state);
-  }, []);
+  const { loading, error, data } = useQuery(GET_PLACES);
+  const places = isFilledArray(data?.places) ? data?.places : null;
 
   return (
     <div className="appWrapper">
       <h1>Fit.me</h1>
-      <h2>Contributors</h2>
-      <p>Jan Hrdy</p>
-      <p>Michael Linka</p>
-      <p>Jaroslav Drahotinsky</p>
-      <p>Tomáš Karásek</p>
-      <p>Tibor Vondrasek</p>
+      <h2>Sport places</h2>
+      {places && places.map((p) => <p>{p.name}</p>) }
     </div>
   );
 }
