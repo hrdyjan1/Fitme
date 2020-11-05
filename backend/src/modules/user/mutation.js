fae5684365ce03f942a05a04987a0c4d3d09b6b7
Jan Hrdý <hrdy@czc.cz>
2020-11-05 22:54:47 +0100

    FM-6 Added mail logic

--- backend/src/modules/user/mutation.js
+++ backend/src/modules/user/mutation.js
@@ -1,16 +1,50 @@
+import nodemailer from 'nodemailer';
+
 import { createToken } from '../../libs/token';
 import { uuidv4 } from '../../constants/uuid';
 import { checkIfValidEmail } from '../../constants/checkIfValidEmail';
 
-export const signin = async (_, { email, password }, { dbConnection }) => {
+const address = 'http://localhost:3000/';
+const sendEmail = async (from, to, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: { user: 'a6c5e91a9dbd75', pass: 'ab98f2fd76bb4e' },
+  });
+
+  return await transporter.sendMail({ from, to, subject, text });
+};
+
+export const verify = async (_, { token }, { dbConnection }) => {
   const user = (
-    await dbConnection.query(`SELECT * FROM user WHERE email = ? AND password = ?`, [email, password])
+    await dbConnection.query(`SELECT * FROM user WHERE verificationToken = ?`, [
+      token,
+    ])
   )[0];
 
-  console.log(user)
-  if(user) {
-      const token = createToken(user);
-      return { user: user, token: token };
+  if (user) {
+    if (!user.verified) {
+      await dbConnection.query(
+        `UPDATE user SET verified = true WHERE verificationToken = ?;`,
+        [token],
+      );
+      return true
+    }
+  }
+  return false
+};
+
+export const signin = async (_, { email, password }, { dbConnection }) => {
+  const user = (
+    await dbConnection.query(
+      `SELECT * FROM user WHERE email = ? AND password = ?`,
+      [email, password],
+    )
+  )[0];
+
+  if (user) {
+    const token = createToken(user);
+    return { user: user, token: token };
   }
 
   throw new Error('Invalidní hodnoty.');
@@ -29,6 +63,14 @@ export const signup = async (
     `INSERT INTO user (id, email, firstName, lastName, password, verificationToken, verified)
       VALUES (?, ?, ?, ?, ?, ?, ?);`,
     [id, email, firstName, lastName, password, verificationToken, 0],
+  );
+
+  const emailText = `Link pro overeni ${address}verificationToken=${verificationToken}`;
+  await sendEmail(
+    '"Fit me 🥇" <no-reply@fitme.com>',
+    email,
+    'Gratuluji',
+    emailText,
   );
 
   const user = { id, email, firstName, lastName, verified: 0 };
