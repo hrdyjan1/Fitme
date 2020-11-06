6dc6f20e4f47f11478baf43bad385726797785e6
Jan Hrdý <hrdy@czc.cz>
2020-11-06 09:13:07 +0100

    Verify

--- frontend/src/pages/PageNotFound.js
+++ frontend/src/pages/PageNotFound.js
@@ -1,6 +1,41 @@
+import { gql, useMutation } from '@apollo/client';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const VERIFY = gql`
+  mutation verifyVoe($token: String!) {
+    verify(token: $token)
+  }
+`;
+
+function Verification({ token }) {
+  const [verify, { data }] = useMutation(VERIFY);
+
+  React.useEffect(() => {
+    verify({ variables: { token } })
+      .then((r) => {
+        if (r.data) {
+          console.log(r.data);
+        }
+        if (r.errors) {
+          console.log(r.errors);
+        }
+      })
+      .catch((e) => alert(e));
+  }, [token, verify]);
+
+  return data?.verify ? <p>Overeno</p> : <p>Problem s overenim</p>;
+}
 
 export function PageNotFound() {
+  const location = useLocation();
+
+  const isVerToken = location.pathname.includes('verificationToken');
+  if (isVerToken) {
+    const token = location.pathname.match(/=(.+)/)[1];
+    return <Verification token={token} />;
+  }
+
   return (
     <div className="appWrapper">
       <h1>Error 404:</h1>
